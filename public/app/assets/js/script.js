87007f5259e9f0a7432c437ca4c72c90e4b3b664
joshuaagapay <jpagapay94@gmail.com>
2018-04-07 04:00:51 +0800

    Second Commit

---
+++ public/app/assets/js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(() => {
+    $('#search').change(function() {
+        const word = $('#search').val();
+        if(word !== ""){
+            $.ajax({url:'https://2-dot-diktoapi.appspot.com/api/v1/words/'+word,
+                method:'GET',
+                success: function(result){
+                    const audio = new Audio(result.audio);
+                    let htmlString = "";
+                    audio.play();
+                    
+                    result.definition.forEach((element) => {
+                        htmlString += `<span class="flow-text">${element.definition}</span></br></br>`; 
+                    });
+
+                    let responseText = `
+                                        <div class="row">
+                                            <div class="col s12"><span class="flow-text"><h3>${result.text}</h3></span></div>
+                                            <div class="col s7 push-s3"><span class="flow-text">${result.translation}</span></div>
+                                            <div class="col s3 pull-s7"><span class="flow-text">Translation:</span></div>
+                                            <div class="col s9 push-s3">${htmlString}</div>
+                                            <div class="col s3 pull-s9"><span class="flow-text">Definition:</span></div>
+                                        </div>
+                                        `;
+
+                    $('.display-word').empty();
+                    $('.display-word').append(responseText);
+                    console.log(result);
+                }
+             });
+        }
+    });
+});
--- server.js
+++ server.js
@@ -2,41 +2,20 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-
 const port = 8000;
 
-app.use('/public', express.static(__dirname + 'public'));
+app.set('views', path.join(__dirname, 'public/app/views'));
+app.set('view engine', 'pug');
 
-app.listen(process.env.PORT || port, function() {
-    console.log('running server');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req,res) => {
+    res.render('index');
 });
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+app.listen(process.env.PORT || port, () => {
+    console.log(`running server at ${port}`);
 });
 
-app.get('/js/materialize.min.js', function(req, res){
-    res.sendFile(__dirname + '/public/app/assets/js/materialize.min.js');
-});
-
-app.get('/css/materialize.min.css', function(req, res){
-    res.sendFile(__dirname + '/public/app/assets/css/materialize.min.css');
-});
-
-app.get('/fonts/roboto/Roboto-Regular.woff2', function(req, res){
-    res.sendFile(__dirname + '/public/app/assets/fonts/roboto/Roboto-Regular.woff2');
-});
-
-app.get('/fonts/roboto/Roboto-Regular.woff', function(req, res){
-    res.sendFile(__dirname + '/public/app/assets/fonts/roboto/Roboto-Regular.woff');
-});
-
-app.get('/fonts/roboto/Roboto-Light.woff2', function(req, res){
-    res.sendFile(__dirname + '/public/app/assets/fonts/roboto/Roboto-Light.woff2');
-});
-
-app.get('/fonts/roboto/Roboto-Light.woff', function(req, res){
-    res.sendFile(__dirname + '/public/app/assets/fonts/roboto/Roboto-Light.woff');
-});
 
 
