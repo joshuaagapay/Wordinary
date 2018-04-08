c04055dcc30ca89065411acd6cbf0e6d5e96a094
joshuaagapay <jpagapay94@gmail.com>
2018-04-09 06:54:06 +0800

    Fifth Commit

--- public/app/assets/js/script.js
+++ public/app/assets/js/script.js
@@ -1,33 +1,52 @@
-$(document).ready(() => {
-    $('#search').change(() => {
-        const word = $('#search').val();
-        if(word !== ""){
-            $.ajax({url:'https://2-dot-diktoapi.appspot.com/api/v1/words/'+word,
-                    method:'GET',
-                    success: (result) => {
-                        const audio = new Audio(result.audio);
-                        let htmlString = "";
-                        audio.play();
+(function () {
+    'use strict';
 
-                        result.definition.forEach((element) => {
-                            htmlString += `<span class="flow-text">${element.definition}</span></br></br>`;
+    $(document).ready(() => {
+        $('#search').change(() => {
+            const word = $('#search').val();
+            const url = `https://2-dot-diktoapi.appspot.com/api/v1/words/${word}`;
+            if(word !== ""){
+
+                const getWords = setInterval(() => {
+                    fetch(url)
+                        .then((response) => {
+                            return response.json();
+                        })
+                        .then((data) => {
+                            displayToHtml(data);
+                            console.log(data);
+                        })
+                        .catch((err) => {
+                            console.log(err);
                         });
-                     
-                        let responseText = `
-                                            <div class="row">
-                                                <div class="col s12"><span class="flow-text"><h3>${result.text}</h3></span></div>
-                                                <div class="col s7 push-s3"><span class="flow-text">${result.translation}</span></div>
-                                                <div class="col s3 pull-s7"><span class="flow-text">Translation:</span></div>
-                                                <div class="col s9 push-s3">${htmlString}</div>
-                                                <div class="col s3 pull-s9"><span class="flow-text">Definition:</span></div>
-                                            </div>
-                                            `;
-                                            
-                        $('.display-word').empty();
-                        $('.display-word').append(responseText);
-                        console.log(result);
-                    }
-            });
-        }
+                        clearInterval(getWords);
+                }, 1000);
+            }
+    
+                const displayToHtml = (data) => {
+    
+                    const audio = new Audio(data.audio);
+                    let htmlString = "";
+                    audio.play();
+    
+                    data.definition.forEach((element) => {
+                        htmlString += `<span class="flow-text"><h6><i>${element.partOfSpeech}<i></h6>${element.definition}.</span></br></br>`;
+                    });
+                    let responseText = `
+                                        <div class="row">
+                                            <div class="col s12"><span class="flow-text"><h3>${data.text}</h3></span></div>
+                                            <div class="col s7 push-s3"><span class="flow-text">${data.translation}</span></div>
+                                            <div class="col s3 pull-s7"><span class="flow-text">Translation:</span></div>
+                                            <div class="col s9 push-s3">${htmlString}</div>
+                                            <div class="col s3 pull-s9"><span class="flow-text">Definition:</span></div>
+                                        </div>
+                                    `;
+                                                    
+                    $('.display-word').empty();
+                    $('.display-word').append(responseText);
+    
+                }
+        });
     });
-});
+    
+}());
